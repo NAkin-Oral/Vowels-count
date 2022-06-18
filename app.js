699f88d7e126d86715e3570767bde96a7f28bb1a
nao <nurettinakinoral@gmail.com>
2022-06-18 15:45:54 +0300

    Better exp

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 document.getElementById('text').addEventListener('input', function (evt) {
   const original = this.value;
-  const count = vowelsCounter4(original);
+  const count = vowelsCounter(original);
   document.getElementById('count').value = count;
 });
 
-const vowelsCounter4 = text => {
+const vowelsCounter = text => {
   let vowels = text.match(/[aeiouıüö]/gi);
   return vowels ? vowels.length : 0;
 };
